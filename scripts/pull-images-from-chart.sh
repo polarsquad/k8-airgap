8a24a8341731c0856c8a993af47586f4d031e366
Camilo Rivera <camilo@riverapineda.com>
2024-02-29 12:44:03 +0200

    read helm from values.yaml, download the docker images, and create airgap vlaues files

--- scripts/pull-images-from-chart.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -x
-helm template gitea-charts/gitea | yq -rN '..|.image? | select(.)' | sort | uniq | xargs -I {} bash -c 'docker save {} | gzip > ../ansible/artifacts/rke2/bootstrap/images/$(echo {} | sed "s/[\.\/]/-/g").tar.gz '
-
